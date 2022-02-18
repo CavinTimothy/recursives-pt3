8c7a7d786c6b1adf75f8e505153c305e90c3748a
CavinTimothy <cavintimotht2014@gmail.com>
2022-02-17 19:43:30 -0800

    10 solved

--- practice-for-week-03-recursion-pt3-practice-main/problems/10-factorial.js
+++ practice-for-week-03-recursion-pt3-practice-main/problems/10-factorial.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Write a recursive function called `factorial` that takes an integer, `num`,
-and returns the factorial of `num`. Assume the value of `num` is greater 
+and returns the factorial of `num`. Assume the value of `num` is greater
 than or equal to 1.
 
 A factorial is the number get when multiplying a number by itself minus one
@@ -16,10 +16,11 @@ factorial(3); // 6
 factorial(5); // 120
 ***********************************************************************/
 
-function factorial(num) {
-  // Your code here
-}
-  
+const factorial = num => num < 2 ? 1 : num * factorial(num - 1);
+
+// function factorial(num) {
+//   // Your code here
+// }
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = factorial;
-  
